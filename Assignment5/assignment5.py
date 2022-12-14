0caec584da7c603d112d02297aea3a2fb4d8fb81
Ruben0595 <76920198+Ruben0595@users.noreply.github.com>
2022-12-14 08:52:51 +0100

    Assignment 5
    finished assignment 5

--- Assignment5/assignment5.py
+++ Assignment5/assignment5.py
@@ -1,199 +1,125 @@
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType,FloatType
-from pyspark.sql.functions import *
-from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.sql import functions as f
 import pandas as pd
 import os
+import csv
+import os
+  
+if not os.path.exists("./output/"):
+    os.makedirs("./output/")
 
-# special thxs to kai for the explain function. :)
-def explain(data):
-    return data._sc._jvm.PythonSQLUtils.explainString(data._jdf.queryExecution(), 'simple')
+sc = SparkContext('local[16]')
+path= "/data/dataprocessing/interproscan/all_bacilli.tsv"
+df = SQLContext(sc).read.csv(path, sep=r'\t', header=False, inferSchema= True)
 
-def spark_df():
-    path = "/data/dataprocessing/interproscan/all_bacilli.tsv"
-    schema = StructType([
-    StructField("Protein_accession", StringType(), True),
-    StructField("Sequence_MD5_digest", StringType(), True),
-    StructField("Sequence_length", IntegerType(), True),
-    StructField("Analysis", StringType(), True),
-    StructField("Signature_accession", StringType(), True),
-    StructField("Signature_description", StringType(), True),
-    StructField("Start_location", IntegerType(), True),
-    StructField("Stop_location", IntegerType(), True),
-    StructField("Score", FloatType(), True),
-    StructField("Status", StringType(), True),
-    StructField("Date", StringType(), True),
-    StructField("InterPro_annotations_accession", StringType(), True),
-    StructField("InterPro_annotations_description", StringType(), True),
-    StructField("GO_annotations", StringType(), True),
-    StructField("Pathways_annotations", StringType(), True)])
-    spark = SparkSession.builder.master("local[16]").appName("InterPro").getOrCreate()
-    return spark.read.option("sep", "\t").option("header", "False").csv(path, schema=schema)
+#i got the column name list from github
+df = df.withColumnRenamed('_c0', 'Protein_accession')\
+    .withColumnRenamed('_c1', 'MD5')\
+    .withColumnRenamed('_c2', 'Seq_len')\
+    .withColumnRenamed('_c3', 'Analysis')\
+    .withColumnRenamed('_c4', 'Signature_accession')\
+    .withColumnRenamed('_c5', 'Signature_description')\
+    .withColumnRenamed('_c6', 'Start')\
+    .withColumnRenamed('_c7', 'Stop')\
+    .withColumnRenamed('_c8', 'Score')\
+    .withColumnRenamed('_c9', 'Status')\
+    .withColumnRenamed('_c10', 'Date')\
+    .withColumnRenamed('_c11', 'InterPro_accession')\
+    .withColumnRenamed('_c12', 'InterPro_description')\
+    .withColumnRenamed('_c13', 'GO_annotations')\
+    .withColumnRenamed('_c14', 'Pathways')
 
-#1 How many distinct protein annotations are found in the dataset? I.e. how many distinc InterPRO numbers are there?
-def q1(df):
-    print("Awsering question 1")
-    data1 = df.select('InterPro_annotations_accession')\
-        .filter(df.InterPro_annotations_accession != "-")\
-        .distinct()
-    exp1 = explain(data1)
-    data1 = data1.count()
-
-    return data1, exp1
-
-#2 How many annotations does a protein have on average?
-def q2(df):
-    print("Awsering question 2")
-    data2 = df.select("Protein_accession",'InterPro_annotations_accession')\
-                .filter(df.InterPro_annotations_accession != "-")\
-                .groupBy("Protein_accession")\
-                .count()\
-                .select(mean("count"))
-    exp2 = explain(data2)      
-    data2 = data2.collect()[0].__getitem__(0)
-
-    return data2, exp2
-
-#3 What is the most common GO Term found?
-def q3(df):
-    print("Awsering question 3")
-    data3 = df.select(df.GO_annotations, explode(split(col("GO_annotations"),"\|"))\
-                        .alias("Split_col"))
-    data3 = data3.filter(data3.Split_col != "-")\
-                .select("Split_col")\
-                .groupby("Split_col")\
-                .count()\
-                .sort("count",ascending=False)
-    exp3 = explain(data3) 
-    data3 = [data[0] for data in data3.take(1)]
-    data3 = data3[0]
-
-    return data3, exp3
-
-#4 What is the average size of an InterPRO feature found in the dataset?
-def q4(df):
-    print("Awsering question 4")
-    data4 = df.withColumn('Sub', (df['Stop_location'] - df['Start_location'])).summary("mean")
-    exp4 = explain(data4)  
-    data4 = data4.collect()[0].__getitem__(-1)
-
-    return data4, exp4
-
-#5 What is the top 10 most common InterPRO features?
-def q5(df):
-    print("Awsering question 5")
-    data5 = df.select('InterPro_annotations_accession')\
-        .filter(df.InterPro_annotations_accession != "-")\
-        .groupBy('InterPro_annotations_accession')\
-        .count()\
-        .sort("count",ascending=False)\
-        .select("InterPro_annotations_accession")
-    exp5 = explain(data5) 
-    data5 = [data[0] for data in data5.take(10)]
-
-    return data5, exp5
-
-#6 If you select InterPRO features that are almost the same size (within 90-100%) as the protein itself, what is the top10 then?
-def q6(df):
-    print("Awsering question 6")
-    data6 = df.select('InterPro_annotations_accession',"Sequence_length",'Stop_location','Start_location')\
-                .filter((df['Stop_location'] - df['Start_location'])/df["Sequence_length"]>=0.9)\
-                .filter(df.InterPro_annotations_accession != "-")\
-                .groupBy('InterPro_annotations_accession')\
-                .count()\
-                .sort("count",ascending=False)\
-                .select("InterPro_annotations_accession")
-    exp6 = explain(data6)
-    data6 = [data[0] for data in data6.take(10)]
- 
-    return data6, exp6
-
-#7 If you look at those features which also have textual annotation, what is the top 10 most common word found in that annotation?
-def q7(df):
-    print("Awsering question 7")
-    data7 = df.select(df.InterPro_annotations_description,explode(split(col("InterPro_annotations_description")," |,"))\
-                .alias("Split_col"))
-    data7 = data7.select("Split_col")\
-                .filter(data7.Split_col != "")\
-                .filter(data7.Split_col != "-")\
-                .groupby("Split_col")\
-                .count()\
-                .sort("count",ascending=False)\
-                .select("Split_col")
-    exp7 = explain(data7)
-    data7 = [data[0] for data in data7.take(10)]
-
-    return data7,exp7
-
-#8 top 10 least common?
-def q8(df):
-    print("Awsering question 8")
-    data8 = df.select(df.InterPro_annotations_description,explode(split(col("InterPro_annotations_description")," |,"))\
-                .alias("Split_col"))
-    data8 = data8.select("Split_col")\
-                .filter(data8.Split_col != "")\
-                .filter(data8.Split_col != "-")\
-                .groupby("Split_col")\
-                .count()\
-                .sort("count",ascending=True)\
-                .select("Split_col")
-    exp8 = explain(data8) 
-    data8 = [data[0] for data in data8.take(10)]
-
-    return data8, exp8
-
-#9 Combining your answers for Q6 and Q7, what are the 10 most commons words found for the largest InterPRO features?
-def q9(df):
-    print("Awsering question 9")
-    data9 = df.select(df.InterPro_annotations_accession,df.InterPro_annotations_description)\
-                .filter(df.InterPro_annotations_accession.isin(data6))\
-                .distinct()
-    data9 = data9.select(data9.InterPro_annotations_description,explode(split(col("InterPro_annotations_description")," |,")))\
-                    .groupby("col")\
-                    .count()
-    data9 = data9.select(data9["col"], data9["count"])\
-                    .filter(data9["col"] != "")\
-                    .sort("count",ascending=False)
-    exp9 = explain(data9)
-    data9 = [data[0] for data in data9.take(10)]
-    
-    return data9, exp9
-
-#10 What is the coefficient of correlation (R^2) between the size of the protein and the number of features found?
-def q10(df):
-    print("Awsering question 10")
-    data10=df.select(df.Protein_accession,df.InterPro_annotations_accession,df.Sequence_length)\
-                .filter(df.InterPro_annotations_accession != "-")\
-                .groupby(df.Protein_accession,"Sequence_length")\
-                .count()
-    exp10 = explain(data10)
-    data10 = data10.corr('Sequence_length', 'count')**2
-
-    return data10, exp10
+#1. How many distinct protein annotations are found in the dataset? I.e. how many distinc InterPRO numbers are there?
+Q1 = df.filter(df.InterPro_accession!="-").select("InterPro_accession").distinct()
+E1 = Q1._sc._jvm.PythonSQLUtils.explainString(Q1._jdf.queryExecution(),"simple")
+A1 = Q1.count()
+print('q1: ', A1)
 
 
-if __name__ == "__main__":
-    df = spark_df()
-    # assign the questions to the functions and the explain data
-    data1, exp1 =  q1(df)
-    data2, exp2 =  q2(df)
-    data3, exp3 =  q3(df)
-    data4, exp4 =  q4(df)
-    data5, exp5 =  q5(df)
-    data6, exp6 =  q6(df)
-    data7, exp7 =  q7(df)
-    data8, exp8 =  q8(df)
-    data9, exp9 =  q9(df)
-    data10,exp10 = q10(df)
-    # create the columns with the awsers, explain data and the question number. 
-    column1 = list(range(1,11))
-    column2 = [data1,data2,data3,data4,data5,data6,data7,data8,data9,data10]
-    column3 = [exp1,exp2,exp3,exp4,exp5,exp6,exp7,exp8,exp9,exp10]
-    # write columns to csv
-    try:
-        d = {'Question': column1, 'Answer': column2,"Explain":column3}
-        df = pd.DataFrame(data = d)
-        if not os.path.exists("output"):
-            os.makedirs("output")
-        df.to_csv("output/output.csv",index=False)
-    except Exception:
-        print("Error while writing to csv")
+#2. How many annotations does a protein have on average?
+Q2 = df.filter(df.InterPro_accession!="-").groupBy('Protein_accession')
+Q2 =Q2.agg(f.count('InterPro_accession')).agg(f.mean('count(InterPro_accession)'))
+E2 = Q2._sc._jvm.PythonSQLUtils.explainString(Q2._jdf.queryExecution(),"simple")
+A2 = Q2.collect()[0][0]
+print('q2: ', A2)
+
+#3. What is the most common GO Term found?
+Q3 = df.filter(df.GO_annotations!="-")
+Q3 = Q3.withColumn("go_a", f.explode(f.split(df.GO_annotations, "[|]")))
+Q3 = Q3.groupBy('go_a').count().orderBy('count', ascending=False)
+E3 = Q3._sc._jvm.PythonSQLUtils.explainString(Q3._jdf.queryExecution(),"simple")
+A3 = Q3.collect()[0][0]
+print('q3: ', A3)
+
+
+#4. What is the average size of an InterPRO feature found in the dataset?
+Q4 = df.withColumn('size', df['Stop'] - df['Start'])
+Q4 = Q4.groupBy().avg('size')
+E4 = Q4._sc._jvm.PythonSQLUtils.explainString(Q4._jdf.queryExecution(),"simple")
+A4 = Q4.collect()[0][0]
+print('q4: ', A4)
+
+#5. What is the top 10 most common InterPRO features?
+Q5 = df.filter(df.InterPro_accession!="-")
+Q5 = Q5.groupBy('InterPro_accession').count().orderBy('count', ascending=False)
+E5 = Q5._sc._jvm.PythonSQLUtils.explainString(Q5._jdf.queryExecution(),"simple")
+A5 = [i[0] for i in Q5.take(10)]
+print('q5: ', A5)
+
+#6. If you select InterPRO features that are almost the same size (within 90-100%) as the protein itself, what is the top10 then?
+Q6 = df.filter(df.InterPro_accession!="-")
+Q6 = Q6.withColumn('size_ipf', Q6['Stop'] - Q6['Start'])
+Q6 = Q6.withColumn('size', Q6['size_ipf']/Q6['Seq_len'])
+Q6 = Q6.filter(Q6['size'] > 0.9).orderBy('size', ascending=False)
+E6 = Q6._sc._jvm.PythonSQLUtils.explainString(Q6._jdf.queryExecution(),"simple")
+A6 = [i[11] for i in Q6.take(10)]
+print('q6: ', A6)
+
+#7. If you look at those features which also have textual annotation, what is the top 10 most common word found in that annotation?
+Q7 = df.filter(df.InterPro_description!="-")
+Q7 = Q7.withColumn("desc", f.explode(f.split(Q7.InterPro_description, " ")))
+Q7 = Q7.groupBy('desc').count().orderBy('count', ascending=False)
+E7 = Q7._sc._jvm.PythonSQLUtils.explainString(Q7._jdf.queryExecution(),"simple")
+A7 = [i[0] for i in Q7.take(10)]
+print('q7: ', A7)
+
+#8. And the top 10 least common?
+Q8 = df.filter(df.InterPro_description!="-")
+Q8 = Q8.withColumn("desc", f.explode(f.split(Q8.InterPro_description, " ")))
+Q8 = Q8.groupBy('desc').count().orderBy('count', ascending=True)
+E8 = Q8._sc._jvm.PythonSQLUtils.explainString(Q7._jdf.queryExecution(),"simple")
+A8 = [i[0] for i in Q8.take(10)]
+print('q8: ', A8)
+
+#9. Combining your answers for Q6 and Q7, what are the 10 most commons words found for the largest InterPRO features?
+Q9 = df.filter(df.InterPro_accession!="-")
+Q9 = Q9.withColumn('size_ipf', Q9['Stop'] - Q9['Start'])
+Q9 = Q9.withColumn('size', Q9['size_ipf']/Q9['Seq_len'])
+Q9 = Q9.filter(Q9['size'] > 0.9).orderBy('size', ascending=False)
+Q9 = Q9.withColumn("desc", f.explode(f.split(Q9.InterPro_description, " ")))
+Q9 = Q9.groupBy('desc').count().orderBy('count', ascending=False)
+E9 = Q9._sc._jvm.PythonSQLUtils.explainString(Q9._jdf.queryExecution(),"simple")
+A9 = [i[0] for i in Q9.take(10)]
+print('q9: ', A9)
+
+#10 What is the coefficient of correlation (R**2) between the size of the protein and the number of features found?
+Q10 = df.filter(df.InterPro_accession!="-")
+Q10 = Q10.groupby(Q10.Protein_accession, 'Seq_len').count()
+E10 = Q10._sc._jvm.PythonSQLUtils.explainString(Q10._jdf.queryExecution(),"simple")
+A10 = Q10.corr('Seq_len', 'count')**2
+print('q10: ', A10)
+
+
+with open('./output/output.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(['1', A1, E1])
+    writer.writerow(['2', A2, E2])
+    writer.writerow(['3', A3, E3])
+    writer.writerow(['4', A4, E4])
+    writer.writerow(['5', A5, E5])
+    writer.writerow(['6', A6, E6])
+    writer.writerow(['7', A7, E7])
+    writer.writerow(['8', A8, E8])
+    writer.writerow(['9', A9, E9])
+    writer.writerow(['10', A10, E10])
